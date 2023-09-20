536e9820c34b6f441a228a8e525f95e4af204e9d
Harold Diaz <harold.diazv1@gmail.com>
2023-09-19 21:37:07 -0500

    Set log-in, hash password feature and save new user

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from '../models/user.interface';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ){}
+
+    @Post('register')
+    registerAccount(@Body() user: User):Observable<User>{
+        return this.authService.registerAccount(user);
+    }
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { Observable, from, map, switchMap } from 'rxjs';
+import { User } from '../models/user.interface';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../models/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ){}
+
+    hashPassword(password: string): Observable<string> {
+        return from(bcrypt.hash(password, 12));
+    }
+
+    registerAccount(user: User): Observable<User>{
+        const { firstName, lastName, email, password } = user;
+        
+        return this.hashPassword(password).pipe(
+            switchMap((hashedPassword: string) => {
+                return from(this.userRepository.save({
+                    firstName,
+                    lastName,
+                    email,
+                    password: hashedPassword
+                })).pipe(
+                    map((user: User) => {
+                        delete user.password;
+                        return user;  
+                    })
+                )
+            })
+        )
+
+    }
+
+}
+
