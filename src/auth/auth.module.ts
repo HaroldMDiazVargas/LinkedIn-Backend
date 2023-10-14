6a8a2d5c6ac3eb959ee6895428b6b79dc23f4912
Harold Diaz <harold.diazv1@gmail.com>
2023-10-14 14:15:33 -0500

    New user service and update interface&entity col imagePath

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserEntity } from './models/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
+// import { JwtModule } from '@nestjs/jwt';
 // import { JwtStrategy } from './strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { SessionSerializer } from './serializers/session.serializer';
+import { UserService } from './services/user.service';
 
 @Module({
   imports: [
@@ -24,8 +25,8 @@ import { SessionSerializer } from './serializers/session.serializer';
     //   })
     // })
   ],
-  providers: [AuthService, LocalStrategy, SessionSerializer], //JwtStrategy
+  providers: [AuthService, LocalStrategy, SessionSerializer, UserService], //JwtStrategy
   controllers: [AuthController],
-  exports: [AuthService]
+  exports: [AuthService, UserService]
 })
 export class AuthModule {}
